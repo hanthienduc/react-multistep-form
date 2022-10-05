139b644fef8b568b7232d7e473f1881e6694b5b7
hanthienduc <hanthienduc.96@gmail.com>
2022-10-05 21:33:40 +0900

    multistep form build with react typescript and custom hooks

---
+++ src/UseForm.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import FormWrapper from './FormWrapper'
+
+type UserData = {
+    firstName: string
+    lastName: string
+    age: string
+}
+
+type UserFormProps = UserData & {
+    updateFields: (fields: Partial<UserData>) => void
+}
+
+function UseForm({ firstName, lastName, age, updateFields }: UserFormProps) {
+    return (
+        <FormWrapper title="User Details">
+            <label>First Name</label>
+            <input
+                autoFocus
+                required
+                type="text"
+                onChange={(e) => updateFields({ firstName: e.target.value })}
+            />
+            <label>Last Name</label>
+            <input
+                required
+                type="text"
+                value={lastName}
+                onChange={(e) => updateFields({ lastName: e.target.value })}
+            />
+            <label>Age</label>
+            <input
+                min={1}
+                required
+                type="number"
+                value={age}
+                onChange={(e) => updateFields({ age: e.target.value })}
+            />
+        </FormWrapper>
+    )
+}
+
+export default UseForm
